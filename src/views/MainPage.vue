37cc42b69d39a199f3cc9f21bdc4b1608b9e42dc
yeon <dalcon10280@gmail.com>
2022-03-20 22:22:09 +0900

    MainPage 퍼블리싱

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,60 +1,162 @@
 <template>
-  <header>
-    <div class="container py-2 mx-auto">
+  <header class="container mx-auto">
+    <div class="flex items-center justify-start h-16 px-4 py-12 mx-auto">
       <h1 class="logo">
         <router-link to="/">
           <img :src="require('@/assets/images/app-logo@2x.png')" alt="탈잉TM" />
         </router-link>
       </h1>
-      <div class="hd_main">
-        <ul class="items-center justify-between flex-1">
-          <li class="link_home act">
-            <a class="underline" href="/">홈<span>HOME</span></a>
-          </li>
-          <li class="link_vod">
-            <a href="/vod/list">브오디<span>VOD</span></a>
-          </li>
-        </ul>
-        <div class="flex-1">
-          <input
-            type="text"
-            class="inp_sch"
-            placeholder="배우고 싶은 재능이나 튜터를 검색해보세요."
-          />
-          <button class="btn_sch"><span class="blind">검색</span></button>
-          <div class="box_keyword">
-            <h2>추천 키워드</h2>
-            <ul class="keyword_list">
-              <li>
-                <a href="#">아트테크</a>
-              </li>
-              <li>
-                <a href="#">비트코인</a>
-              </li>
-              <li>
-                <a href="#">영어자격증</a>
-              </li>
-              <li>
-                <a href="#">신학기_디자인영상</a>
-              </li>
-              <li>
-                <a href="#">유튜브_제작</a>
-              </li>
-              <li>
-                <a href="#">인스타_관리</a>
-              </li>
-              <li>
-                <a href="#">부동산/프롭테크</a>
-              </li>
-              <li>
-                <a href="#">영어일기쓰기_챌린지</a>
-              </li>
-            </ul>
+
+      <div
+        class="flex items-center justify-start w-full h-16 m-2 border border-gray-100 rounded-md"
+      >
+        <nav class="flex items-center justify-center mx-10 space-x-8 text-sm font-medium">
+          <a class="py-1 font-bold text-gray-900 border-b-2 border-black" href="">홈 HOME</a>
+          <a class="font-bold text-gray-900" href="">브오디 VOD</a>
+        </nav>
+
+        <form class="hidden mb-0 lg:flex">
+          <div class="relative">
+            <input
+              class="h-10 text-sm border-2 border-gray-200 rounded-3xl pr-60 focus:z-10"
+              type="text"
+            />
+
+            <button
+              class="absolute inset-y-0 right-0 p-2 mr-px text-gray-600 rounded-r-lg"
+              type="submit"
+            >
+              <svg
+                class="w-5 h-5"
+                fill="currentColor"
+                viewbox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clip-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  fill-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
           </div>
-        </div>
+        </form>
+      </div>
+
+      <div class="flex justify-end flex-1 w-0 lg:hidden">
+        <button class="p-2 text-gray-500 bg-gray-100 rounded-full" type="button">
+          <svg
+            class="w-5 h-5"
+            fill="currentColor"
+            viewbox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
   </header>
+
+  <ul class="flex items-center justify-start w-full h-16 m-2 border border-gray-100 rounded-md">
+    <li class="flex-1">
+      <a class="relative block p-4" href="">
+        <span class="absolute inset-x-0 w-full h-px bg-pink-600 border-2 -bottom-px"></span>
+
+        <div class="flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="flex-shrink-0 w-5 h-5 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+            />
+          </svg>
+
+          <span class="ml-3 text-sm font-medium text-gray-900"> Settings </span>
+        </div>
+      </a>
+    </li>
+
+    <li class="flex-1">
+      <a class="relative block p-4" href="">
+        <div class="flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="flex-shrink-0 w-5 h-5 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+            />
+          </svg>
+
+          <span class="ml-3 text-sm font-medium text-gray-900"> Messages </span>
+        </div>
+      </a>
+    </li>
+
+    <li class="flex-1">
+      <a class="relative block p-4" href="">
+        <div class="flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="flex-shrink-0 w-5 h-5 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+            />
+          </svg>
+
+          <span class="ml-3 text-sm font-medium text-gray-900"> Archive </span>
+        </div>
+      </a>
+    </li>
+
+    <li class="flex-1">
+      <a class="relative block p-4" href="">
+        <div class="flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="flex-shrink-0 w-5 h-5 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+
+          <span class="ml-3 text-sm font-medium text-gray-900"> Notifications </span>
+        </div>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts"></script>
