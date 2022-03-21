f339312fd55b3fa1a04c6d5db183f6432aa55cde
Dal-ya <hyansky@daum.net>
2022-03-21 21:14:03 +0900

    search class page

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -2,8 +2,20 @@
   <div>
     <NavBar />
     <main>
-      <div class="page-container">
-        <SearchClassCard />
+      <div class="page-fit-container">
+        <div class="mt-5 mb-5 text-sm text-gray-600">7376개의 수업</div>
+        <!-- search class -->
+        <div class="mb-20 grid grid-cols-3 gap-5">
+          <div :key="idx" v-for="(_, idx) in new Array(15)">
+            <router-link to="#">
+              <SearchClassCard />
+            </router-link>
+          </div>
+        </div>
+        <!-- pagination -->
+        <div class="mb-20">
+          <SearchClassPagination class="mx-auto" />
+        </div>
       </div>
     </main>
     <Footer />
@@ -14,6 +26,11 @@
 import NavBar from '@/components/Base/NavBar.vue';
 import Footer from '@/components/Base/Footer.vue';
 import SearchClassCard from '@/components/Search/SearchClassCard.vue';
+import SearchClassPagination from '@/components/Search/SearchClassPagination.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.active {
+  @apply border border-gray-400 text-red-500;
+}
+</style>
